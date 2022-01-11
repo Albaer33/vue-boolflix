4929d2d0885431a43c64fe32dce334a5ee8512ff
Albaer33 <alisi3396@gmail.com>
2022-01-11 15:24:54 +0100

    setting up all components

---
+++ src/components/Card.vue
@@ -0,0 +1,18 @@
+<template>
+    <div>ciao sono una card</div>
+</template>
+
+<script>
+export default {
+    name: 'Card',
+}
+</script>
+
+<style scoped lang='scss'>
+main {
+    width: 100%;
+    height: 80px;
+    padding: 20px;
+    background-color: lightgray;
+}
+</style>
---
+++ src/components/Header.vue
@@ -0,0 +1,21 @@
+<template>
+    <header>
+        <input type="text">
+        <button>Cerca</button>
+    </header>
+</template>
+
+<script>
+export default {
+    name: 'Header',
+}
+</script>
+
+<style scoped lang='scss'>
+header {
+    width: 100%;
+    height: 80px;
+    padding: 20px;
+    background-color: pink;
+}
+</style>
