11145ddf95e8a8a727f1942dae192bcd5c700b4d
Albaer33 <alisi3396@gmail.com>
2022-01-11 16:38:37 +0100

    cardlist component created

--- src/App.vue
+++ src/App.vue
@@ -1,41 +1,19 @@
 <template>
   <div id="app">
     <Header/>
-    <main>
-      <Card/>
-    </main>
+    <Cardlist/>
   </div>
 </template>
 
 <script>
 import Header from './components/Header.vue'
-import Card from './components/Card.vue'
-import axios from 'axios';
+import Cardlist from './components/Cardlist.vue'
 
 export default {
   name: 'App',
   components: {
     Header,
-    Card
-  },
-  data: function() {
-        return {
-           searchedWord: 'vita',
-           filmArray: [],
-        }
-  },
-  created: function() {
-    axios.get('https://api.themoviedb.org/3/search/movie',
-      {
-        params: {
-          api_key: '62bbed7da31113bfdbc2205370dfec35',
-          query: searchedWord,
-        }
-      }
-    )
-      .then((response) => {
-        this.filmArray = response.data.response;
-      });
+    Cardlist,
   },
 }
 </script>
@@ -45,10 +23,5 @@ export default {
   padding: 0;
   margin: 0;
   box-sizing: border-box;
-}
-main {
-    width: 100%;
-    padding: 20px;
-    background-color: lightgray;
 }
 </style>
---
+++ src/components/Cardlist.vue
@@ -0,0 +1,43 @@
+<template>
+    <main>
+        <Card/>
+    </main>
+</template>
+
+<script>
+import Card from './Card.vue'
+import axios from 'axios';
+
+export default {
+    components: {
+        Card
+    },
+    name: 'Cardlist',
+    data: function() {
+        return {
+            searchedWord: 'vita',
+            filmArray: [],
+        }
+    },
+    created: function() {
+        axios.get('https://api.themoviedb.org/3/search/movie',
+        {
+            params: {
+                api_key: '62bbed7da31113bfdbc2205370dfec35',
+                query: searchedWord,
+            }
+        })
+        .then((response) => {
+            this.filmArray = response.data.response;
+        });
+    },
+}
+</script>
+
+<style scoped lang='scss'>
+main {
+    width: 100%;
+    padding: 20px;
+    background-color: lightgray;
+}
+</style>
