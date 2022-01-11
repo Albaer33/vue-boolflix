503f647be7adcb11b2619350cda1c9ac1d4c9a18
Albaer33 <alisi3396@gmail.com>
2022-01-11 17:29:58 +0100

    removed cardlist components

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,40 @@
 <template>
   <div id="app">
     <Header/>
-    <Cardlist/>
+    <main>
+      <Card/>
+    </main>
   </div>
 </template>
 
 <script>
-import Header from './components/Header.vue'
-import Cardlist from './components/Cardlist.vue'
+import Header from './components/Header.vue';
+import Card from './components/Card.vue';
+import axios from 'axios';
 
 export default {
   name: 'App',
   components: {
     Header,
-    Cardlist,
+    Card,
+  },
+  data: function() {
+    return {
+      searchedWord: 'ritorno al futuro',
+      filmArray: [],
+      }
+    },
+  created: function() {
+    axios.get('https://api.themoviedb.org/3/search/movie',
+    {
+      params: {
+        api_key: '62bbed7da31113bfdbc2205370dfec35',
+        query: this.searchedWord,
+      }
+    })
+    .then((response) => {
+      this.filmArray = response.data.results;
+    });
   },
 }
 </script>
@@ -23,5 +44,10 @@ export default {
   padding: 0;
   margin: 0;
   box-sizing: border-box;
+}
+main {
+    width: 100%;
+    padding: 20px;
+    background-color: lightgray;
 }
 </style>
